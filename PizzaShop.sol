a465e5a9f213b786749c4d6a46f4baacdc926fe0
Niluk <67406138+niluk-256@users.noreply.github.com>
2022-07-16 14:30:39 +0530

    Update PizzaShop.sol

--- PizzaShop.sol
+++ PizzaShop.sol
@@ -1,1 +1,51 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract PizzaShop {
 
+event buyPizza(address indexed _from,uint256 cost);
+
+uint256  public  pizza_cost = 0.0001 ether;
+
+
+function  buyaPizza()  payable public  returns (uint256) { 
+  
+ emit buyPizza(msg.sender, pizza_cost);
+
+return pizza_cost;
+
+ 
+ }
+
+  struct Man {
+      string name;
+      uint age;
+  }
+
+ Man[] public  man;
+
+
+ function adddetails(uint256 _age , string memory _name) public {
+
+ man.push(Man({age:_age , name:_name})); //this is correct
+ // man.push(Man(_age ,_name)); //this doesnt work why?
+
+ }
+
+function addperson() public {
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
